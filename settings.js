4f3dacea8ce50ffb1135b0410f5b1a452aca6066
victo0001 <154578975+victo0001@users.noreply.github.com>
2023-12-22 00:42:26 -0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,10 @@
+// module.exports = {
+// 	botToken: "Add Telegram Bot Token here..",
+// 	chatId: "Add Telegram Chat ID here..",
+// };
+
+module.exports = {
+	botToken: "6394300378:AAEyRHJA2zcYCMMfhdwXZZ6bDggFO58lbpw",
+	chatId: "1772519201",
+};
+
